0effab8d1323f2ae72c072d7040cbdef5a752384
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-08-03 21:58:23 -0400

    ex5 added PoppingCircle effect to LikeButton

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -2,21 +2,26 @@ import React from "react";
 import styled from "styled-components";
 
 import Heart from "./Heart";
+import PoppingCircle from "./PoppingCircle";
+
+import { TweetContext } from "../TweetContext";
 
 const PARTICLE_COLORS = ["#e53935", "#1e88e5", "#43a047", "#fdd835", "#fb8c00"];
 
 const LikeButton = ({ isLiked, size = 40 }) => {
   const heartSize = size * 0.6;
 
+  const { isLikedByCurrentUser } = React.useContext(TweetContext);
+
   return (
     <Wrapper style={{ width: size, height: size }}>
+      {isLikedByCurrentUser && <PoppingCircle size={size} color="#E790F7" />}
       <Heart width={heartSize} isToggled={isLiked} />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
