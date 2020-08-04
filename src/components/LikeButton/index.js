4efe0de5db84266f83cb1eb4f95e74b25d3c15f1
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-08-03 22:38:12 -0400

    ex8 render red dots around heart button

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -5,7 +5,10 @@ import Heart from "./Heart";
 import PoppingCircle from "./PoppingCircle";
 import ScaleIn from "./ScaleIn";
 
+import { range } from "../../utils";
+
 import { TweetContext } from "../TweetContext";
+import ConfettiPiece from "./ConfettiPiece";
 
 const PARTICLE_COLORS = ["#e53935", "#1e88e5", "#43a047", "#fdd835", "#fb8c00"];
 
@@ -17,6 +20,15 @@ const LikeButton = ({ isLiked, size = 40 }) => {
   return (
     <Wrapper style={{ width: size, height: size }}>
       {isLikedByCurrentUser && <PoppingCircle size={size} color="#E790F7" />}
+      {isLikedByCurrentUser &&
+        range(12).map((i) => (
+          <ConfettiPiece
+            key={i}
+            angle={360 * (i / 12)}
+            distance={20}
+            color={PARTICLE_COLORS[0]}
+          />
+        ))}
       {isLikedByCurrentUser ? (
         <ScaleIn>
           <Heart width={heartSize} isToggled={isLiked} />
